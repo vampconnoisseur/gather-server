475221ee0ac3dda96cc13282620177283f2f8796
Jaiditya Batra <jaiditya7318@gmail.com>
2023-10-02 22:56:22 +0530

    final commit

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const { RtcTokenBuilder, RtcRole, RtmTokenBuilder, RtmRole } = require('agora-token');
+const { RtcTokenBuilder, RtcRole, RtmTokenBuilder, RtmRole } = require('agora-access-token');
 
 dotenv.config();
 const app = express();
@@ -8,7 +8,16 @@ const PORT = process.env.PORT || 8080;
 const APP_ID = process.env.APP_ID;
 const APP_CERTIFICATE = process.env.APP_CERTIFICATE;
 
+const nocache = (_, resp, next) => {
+    resp.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+    resp.header('Expires', '-1');
+    resp.header('Pragma', 'no-cache');
+    next();
+}
+
+
 const generateRTCToken = (req, resp) => {
+    resp.header('Access-Control-Allow-Origin', '*');
 
     const channelName = req.params.channel;
     if (!channelName) {
@@ -27,15 +36,15 @@ const generateRTCToken = (req, resp) => {
     const currentTime = Math.floor(Date.now() / 1000);
     const privilegeExpireTime = currentTime + expireTime;
 
-    let token = RtcTokenBuilder.buildTokenWithUid(APP_ID, APP_CERTIFICATE, channelName, uid, role, 0, privilegeExpireTime);
+    let token = RtcTokenBuilder.buildTokenWithUid(APP_ID, APP_CERTIFICATE, channelName, uid, role, privilegeExpireTime);
 
     return resp.json({ 'rtcToken': token });
 }
 
-app.get('/rtc/:channel/:uid', generateRTCToken);
+app.get('/rtc/:channel/:uid', nocache, generateRTCToken);
 
 app.get('/', function (req, res) {
-    console.log("Render Working")
+    console.log("Render Working");
 });
 
 app.listen(PORT, () => {
